bc5880c3649c0a9d3ce39bf91aac8d5b4df7441a
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-04 15:36:55 +0900

    Add routes about questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/home' => 'static_pages#home'
+
+  resources :questions
 end
