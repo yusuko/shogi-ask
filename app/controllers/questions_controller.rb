c9a12497e457d0ea5e1f7838e38814bb5a7bc0f6
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-05 19:14:57 +0900

    Create questions#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,10 @@ class QuestionsController < ApplicationController
     redirect_after_question_create(question)
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
 
   def questions_params
