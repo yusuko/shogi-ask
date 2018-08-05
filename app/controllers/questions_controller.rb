deef726d603967a02c29578fa2cb106e7a8e1f08
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-05 18:39:30 +0900

    Add flash message when question is posted

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,13 +7,23 @@ class QuestionsController < ApplicationController
 
   def create
     question = Question.new(questions_params)
-    redirect_to(root_url) && return if question.save
-    render 'questions#new'
+    redirect_after_question_create(question)
   end
 
   private
 
   def questions_params
     params.require(:question).permit(:title, :content)
+  end
+
+  def redirect_after_question_create(question)
+    if question.save
+      flash[:success] = '質問が投稿されました!'
+      redirect_to(root_url) && return
+    else
+      flash[:danger] = question.errors.full_messages
+      @question = Question.new
+      render 'new'
+    end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
 end
