69c4aa2fca1d126859f77029e53645a5158de020
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-09 13:23:21 +0900

    Add associations between user and questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new(question_params)
+    question = Question.new(question_params.merge(user_id: current_user.id))
     redirect_after_question_create(question)
   end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :replies
+  belongs_to :user
 
   validates :content, presence: true
   validates :title, presence: true
