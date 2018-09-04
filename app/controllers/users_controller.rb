63ec4c892b760bdda23a1cac5090268b514638e9
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-04 19:17:45 +0900

    Change redirect when updating profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def edit_profile
     current_user.update(profile_params)
-    redirect_to users_setting_profile_path if current_user.save
+    redirect_after_update_profile
   end
 
   def show
@@ -16,5 +16,15 @@ class UsersController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:name, :sentence, :image)
+  end
+
+  def redirect_after_update_profile
+    if current_user.save
+      flash[:success]="プロフィールが更新されました。"
+      redirect_to current_user
+    else
+      flash[:danger]="プロフィールが正しく更新されませんでした。"
+      redirect_to current_user
+    end
   end
 end
