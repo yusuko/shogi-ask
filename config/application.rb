d26e4748573a432ee0f7fa2bfb2b205dd27e0658
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-04 17:40:13 +0900

    Add color to devise flash and change message to japanese

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module ShareCook
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
