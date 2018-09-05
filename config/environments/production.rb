e7f6a93b967b521ef1762358afed9fa733779dac
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-05 12:29:24 +0900

    Change mailer setting folowed by changing URI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://powerful-chamber-66075.herokuapp.com/'
+  host = 'https://shogi-ask.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
