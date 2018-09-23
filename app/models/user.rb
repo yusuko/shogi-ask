f06eff7f7c7c6f55cff9e9693d22c017ef287458
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-23 17:34:02 +0900

    Add some settings to perform twitter login successfully in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
-      user.remote_image_url = auth.info.image
+      user.remote_image_url = auth.info.image.gsub('http', 'https')
       user.skip_confirmation!
     end
   end
